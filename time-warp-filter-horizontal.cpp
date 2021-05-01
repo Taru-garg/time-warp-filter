096eee99090ddd9926d7cc81c58a6b2df63a6a8b
Taru Garg <tarugarg2@gmail.com>
2021-05-01 17:28:34 +0530

    Add files via upload

---
+++ time-warp-filter-horizontal.cpp
@@ -0,0 +1,56 @@
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+using namespace cv;
+using namespace std;
+int main(int, char**)
+{
+    Mat frame;
+    //--- INITIALIZE VIDEOCAPTURE
+    VideoCapture cap;
+    // open the default camera using default API
+    // cap.open(0);
+    // OR advance usage: select any API backend
+    int deviceID = 0;             // 0 = open default camera
+    int apiID = cv::CAP_ANY;      // 0 = autodetect default API
+    // open selected camera using selected API
+    cap.open(deviceID, apiID);
+    // check if we succeeded
+    if (!cap.isOpened()) {
+        cerr << "ERROR! Unable to open camera\n";
+        return -1;
+    }
+    //--- GRAB AND WRITE LOOP
+    cout << "Start grabbing" << endl
+        << "Press any key to terminate" << endl;
+    
+    cv::Mat processedPhoto(480, 640, CV_8UC3);
+    int num_rows = 0;
+    bool flag = false;
+    while(true)
+    {
+        // wait for a new frame from camera and store it into 'frame'
+        cap.read(frame);
+        // check if we succeeded
+        if (frame.empty()) {
+            cerr << "ERROR! blank frame grabbed\n";
+            break;
+        }
+        // show live and wait for a key with timeout long enough to show images
+        if(num_rows < frame.rows)
+        frame.row(num_rows).copyTo(processedPhoto.row(num_rows));
+        num_rows++;
+        // processedPhoto = frame;
+        imshow("Live", frame);
+        if(!flag) sleep(5), flag = true;
+        imshow("Processed", processedPhoto);
+        if (waitKey(1) >= 0)
+            break;
+    }
+    
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
+}
